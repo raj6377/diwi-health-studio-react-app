1233cc40c1fc55bcb1b186f9dca741565e9ced29
renezn <abhi900vic@gmail.com>
2022-11-02 13:07:48 +0530

    changed about

--- src/About/About.js
+++ src/About/About.js
@@ -1,7 +1,37 @@
 import React from 'react'
+import './About.css'
+import pic1 from '../Images/home-bg.jpg'
+import general from '../Images/4.jpg'
+import cosmetic from '../Images/5.jpg'
+import surgical from '../Images/6.jpg'
 
-export default function About() {
+
+const About = ()=>{
   return (
-    <h1>About</h1>
+    <>
+    <div className='outer'>
+
+      
+       <div className='main'>
+      <div className='box'>
+      <h2>About Us</h2>
+      <br/>
+      <p>Have to write something here. 
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it to
+       make a type specimen book. It has sur
+       vived not only five centuries, but also the leap i
+       
+      </p>
+    </div>
+    <img src={pic1} className="doc-pic" alt='doc-pic'/>
+    
+    </div>
+    </div>
+  </>
+   
   )
 }
+
+export default About;
