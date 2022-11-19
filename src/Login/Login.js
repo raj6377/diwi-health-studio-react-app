dd519afbd44fe4eb1c79e55861adf993e391920c
renezn <abhi900vic@gmail.com>
2022-11-19 15:18:25 +0530

    added login

---
+++ src/Login/Login.js
@@ -0,0 +1,42 @@
+import './login.css'
+import { useContext, useState } from 'react';
+import {signInWithEmailAndPassword} from 'firebase/auth'
+import {auth} from '../Firebase-config/Firebase-config'
+import { useNavigate } from 'react-router-dom';
+// import { AuthContext } from '../context/AuthContext';
+
+const Login = () =>{
+    const [error,setError]=useState(false);
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    const navigate=useNavigate();
+    // const {dispatch}=useContext(AuthContext)
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+        //   dispatch({type:"LOGIN",payload:user})
+          navigate("/Admin");
+        })
+        .catch((error) => {
+          setError(true);
+        });
+    }
+
+ return (
+        <div className='login'>
+            <form onSubmit={handleSubmit}>
+            <img src={require('../Images/logo1.png')}/>
+                <input type='email' placeholder='email' onChange={(e)=>setEmail(e.target.value)}></input>
+                <input type='password' placeholder='password' onChange={(e)=>setPassword(e.target.value)}></input>
+                <button type='submit'>Login</button>
+                {error && <span>Wrong username or password!</span>}
+            </form>
+        </div>
+    )
+}
+export default Login;
