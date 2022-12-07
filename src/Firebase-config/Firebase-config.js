910e4d27573fa85babd1f580099450a6288eda5b
Raj <coder982923@gmail.com>
2022-12-07 13:10:00 +0530

    project db changed

--- src/Firebase-config/Firebase-config.js
+++ src/Firebase-config/Firebase-config.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore'
+import { getAuth } from 'firebase/auth'
 
 
 // const firebaseConfig = {
@@ -12,16 +13,16 @@ import { getFirestore } from "@firebase/firestore"
 //     measurementId: "G-LN8ZSKSRJB"
 //   };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCK3nag5DHrcHOfYTCh09e1xu6iS8_P70o",
-  authDomain: "diwi-health-studio.firebaseapp.com",
-  projectId: "diwi-health-studio",
-  storageBucket: "diwi-health-studio.appspot.com",
-  messagingSenderId: "89321223298",
-  appId: "1:89321223298:web:45f9df7c70f09aa486309b",
-  measurementId: "G-LN8ZSKSRJB"
+const firebaseApp = {
+  apiKey: "AIzaSyDPV_WB3_KzsKPhJX9D8AtaT9YBRFzdWMs",
+  authDomain: "diwi-health-studio-370306.firebaseapp.com",
+  projectId: "diwi-health-studio-370306",
+  storageBucket: "diwi-health-studio-370306.appspot.com",
+  messagingSenderId: "255046250570",
+  appId: "1:255046250570:web:e89138593938362ee4f6b3",
+  measurementId: "G-BMGFY25V7F"
 };
 
-  const app = initializeApp(firebaseConfig);
-
-  export const db = getFirestore(app);
+  const db = firebaseApp.firestore();
+  export{db};
+  export const auth = getAuth();
