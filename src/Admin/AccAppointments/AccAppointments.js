81cdbdddab01cc591ccfbd1d67bea91325e47a22
renezn <abhi900vic@gmail.com>
2022-12-27 19:08:47 +0530

    sorting

--- src/Admin/AccAppointments/AccAppointments.js
+++ src/Admin/AccAppointments/AccAppointments.js
@@ -1,150 +1,241 @@
-import React, { useState, useEffect} from 'react'
-import { db } from '../../Firebase-config/Firebase-config';
-import { Link } from 'react-router-dom';
-import { addDoc, collection } from 'firebase/firestore'
-import AppNav from '../AppointmentNav/AppointmentNav'
+import React, { useState, useEffect } from "react";
+import { db } from "../../Firebase-config/Firebase-config";
+import { Link } from "react-router-dom";
+import { addDoc, collection } from "firebase/firestore";
+import AppNav from "../AppointmentNav/AppointmentNav";
 
-export default function AccAppointments( props ) {
+export default function AccAppointments(props) {
+  const [data, setData] = useState([]);
+  const [sortedData, setSortedData] = useState([]);
+  const [sort, setSort] = useState(false);
 
-    const [data, setData] = useState([]);
+  // const {Address,DateRequested,Age,Gender,Mail,Mobile,Name,Purpose,TimeRequested} = data;
+  var Address = "";
+  var DateRequested = "";
+  var Age = "";
+  var Gender = "";
+  var Mail = "";
+  var Mobile = "";
+  var Name = "";
+  var Purpose = "";
+  var TimeRequested = "";
 
-    // const {Address,DateRequested,Age,Gender,Mail,Mobile,Name,Purpose,TimeRequested} = data;
-    var Address = '';
-    var DateRequested = '';
-    var Age = '';
-    var Gender = '';
-    var Mail = '';
-    var Mobile = '';
-    var Name = '';
-    var Purpose = '';
-    var TimeRequested = '';
-    
-    useEffect(()=>{
-        props.setNavShow(false)
-    },[props.setNavShow])
+  useEffect(() => {
+    props.setNavShow(false);
+  }, [props.setNavShow]);
 
-    useEffect(() => {
-        db.collection('AccAppointment').onSnapshot((snapshot) => {
-            const AccAppointments = [];
-            snapshot.forEach((doc) => {
-                AccAppointments.push({ ...doc.data(), id: doc.id });
-            });
+  useEffect(() => {
+    db.collection("AccAppointment").onSnapshot((snapshot) => {
+      const AccAppointments = [];
+      snapshot.forEach((doc) => {
+        AccAppointments.push({ ...doc.data(), id: doc.id });
+      });
 
-            setData(AccAppointments);
-            // console.log(data);
-        })
-    }, []);
+      setData(AccAppointments);
+      // console.log(data);
+    });
+  }, []);
 
-    const onDelete = (id) => {
-        if (window.confirm("Are you sure you want to delete?")) {
-            db.collection('AccAppointment').doc(id).delete();
-        }
+  const onDelete = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      db.collection("AccAppointment").doc(id).delete();
     }
+  };
 
-    const PastAppointmentCollectionRef = collection(db,"PastAppointment");
+  const PastAppointmentCollectionRef = collection(db, "PastAppointment");
 
-    const pushToPastAppointments = async (id) => {
+  const pushToPastAppointments = async (id) => {
+    var ind = 0;
 
+    data.forEach((block) => {
+      if (block.id === id) {
+        Address = block.Address;
+        Name = block.Name;
+        Age = block.Age;
+        Mail = block.Mail;
+        Mobile = block.Mobile;
+        Purpose = block.Purpose;
+        TimeRequested = block.TimeRequested;
+        DateRequested = block.DateRequested;
+        Gender = block.Gender;
+        ind = data.indexOf(block);
+      }
+    });
 
-        var ind = 0
+    db.collection("AccAppointment").doc(data[ind].id).delete();
 
-        data.forEach((block) => {
-            if(block.id===id){
-                Address=block.Address
-                Name = block.Name
-                Age = block.Age
-                Mail=block.Mail
-                Mobile = block.Mobile
-                Purpose = block.Purpose
-                TimeRequested = block.TimeRequested
-                DateRequested = block.DateRequested
-                Gender=block.Gender
-                ind = data.indexOf(block)
-            }
-        });        
+    await addDoc(PastAppointmentCollectionRef, {
+      Name: Name,
+      Address: Address,
+      Age: Age,
+      Gender: Gender,
+      Mail: Mail,
+      Mobile: Mobile,
+      Purpose: Purpose,
+      TimeRequested: TimeRequested,
+      DateRequested: DateRequested,
+    });
+  };
 
-        db.collection('AccAppointment').doc(data[ind].id).delete();
-
-        await addDoc(PastAppointmentCollectionRef,{Name:Name,Address:Address,Age:Age,Gender:Gender,Mail:Mail,Mobile:Mobile,Purpose:Purpose,TimeRequested:TimeRequested,DateRequested:DateRequested})
-       
-    }
-
+  const handleChange = (e) => {
+    setSort(true);
+    db.collection("AccAppointment")
+      .orderBy(`${e.target.value}`)
+      .onSnapshot((snapshot) => {
+        let sortedData = [];
+        snapshot.forEach((snap) => {
+          sortedData.push({ ...snap.data(), id: snap.id });
+        });
+        setSortedData(sortedData);
+      });
+  };
+  const handleReset = () => {
+    setSort(false);
+  };
 
   return (
-    <div><div className="table-outer-container">
-    <div className='emp-div-nav'> <h1>Appointments</h1> </div>
-    <div className="table-inner-container1">
-        <div className="table-inner-container2">
+    <div>
+      <div className="table-outer-container">
+        <div className="emp-div-nav">
+          {" "}
+          <h1>Appointments</h1>{" "}
+        </div>
+        <div className="table-inner-container1">
+          <div className="table-inner-container2">
             <div className="table-heading">
-                <h1>{<AppNav/>}</h1>
+              <h1>{<AppNav />}</h1>
             </div>
-
+            <label>Sort By:</label>
+            <select
+              className="dropdown"
+              name="colValue"
+              onChange={handleChange}
+            >
+              <option>Please Select</option>
+              <option value="Name">Name</option>
+              <option value="TimeRequested">Time Requested</option>
+              <option value="DateRequested">Date Requested</option>
+            </select>
+            <button className="btn btn-dark" onClick={handleReset}>
+              Reset
+            </button>
+            <br />
             <table className="table-content">
-
-                <thead className='thead-dark'>
-                    <tr>
-                        <th >No.</th>
-                        <th >Name</th>
-                        <th >Mobile</th>
-                        <th >Mail</th>
-                        <th >Address</th>
-                        <th >Age</th>
-                        <th >Gender</th>
-                        <th >Date Requested</th>
-                        <th >Time Requested</th>
-                        <th >Purpose</th>
-                        <th >Action</th>
-                    </tr>
-                </thead>
-
+              <thead className="thead-dark">
+                <tr>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Mobile</th>
+                  <th>Mail</th>
+                  <th>Address</th>
+                  <th>Age</th>
+                  <th>Gender</th>
+                  <th>Date Requested</th>
+                  <th>Time Requested</th>
+                  <th>Purpose</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              {!sort && (
                 <tbody>
-                    {
-                    data.map((item, index) => {
-                        let even = false
+                  {data.map((item, index) => {
+                    let even = false;
 
-                            if((index+1)%2===0){
-                                even = true
-                            }
-                            else{
-                                even = false
-                            }
+                    if ((index + 1) % 2 === 0) {
+                      even = true;
+                    } else {
+                      even = false;
+                    }
 
-                        return (
-                            <tr key={item.id} className={`${even ? 'even-row':''}`}>
-                                <th >{index + 1}</th>
-                                <td>{item.Name}</td>
-                                <td>{item.Mobile}</td>
-                                <td>{item.Mail}</td>
-                                <td>{item.Address}</td>
-                                <td>{item.Age}</td>
-                                <td>{item.Gender}</td>
-                                <td>{item.DateRequested}</td>
-                                <td>{item.TimeRequested}</td>
-                                <td>{item.Purpose}</td>
-                                <td className='new-record-operations'>
-                                    {/* <Link to={`/update/${item.id}`}>
+                    return (
+                      <tr key={item.id} className={`${even ? "even-row" : ""}`}>
+                        <th>{index + 1}</th>
+                        <td>{item.Name}</td>
+                        <td>{item.Mobile}</td>
+                        <td>{item.Mail}</td>
+                        <td>{item.Address}</td>
+                        <td>{item.Age}</td>
+                        <td>{item.Gender}</td>
+                        <td>{item.DateRequested}</td>
+                        <td>{item.TimeRequested}</td>
+                        <td>{item.Purpose}</td>
+                        <td className="new-record-operations">
+                          {/* <Link to={`/update/${item.id}`}>
                                         <a >
                                             <i className="fas fa-pencil-alt fa-lg update-btn" />
                                         </a>
                                     </Link> */}
-                                    <a >
-                                        <i onClick={() => onDelete(item.id)} className="fas fa-trash-alt fa-lg delete-btn" />
-                                    </a>
-                                    
-                                    <a >
-                                        <i onClick={() => pushToPastAppointments(item.id)} className="fa-sharp fa-solid fa-check fa-lg tick-btn" />
-                                    </a>
-                                    
+                          <a>
+                            <i
+                              onClick={() => onDelete(item.id)}
+                              className="fas fa-trash-alt fa-lg delete-btn"
+                            />
+                          </a>
 
-                                </td>
-                            </tr>
-                        );
-                    })
-                    }
+                          <a>
+                            <i
+                              onClick={() => pushToPastAppointments(item.id)}
+                              className="fa-sharp fa-solid fa-check fa-lg tick-btn"
+                            />
+                          </a>
+                        </td>
+                      </tr>
+                    );
+                  })}
                 </tbody>
+              )}
+              {sort && (
+                <tbody>
+                  {sortedData.map((item, index) => {
+                    let even = false;
+
+                    if ((index + 1) % 2 === 0) {
+                      even = true;
+                    } else {
+                      even = false;
+                    }
+
+                    return (
+                      <tr key={item.id} className={`${even ? "even-row" : ""}`}>
+                        <th>{index + 1}</th>
+                        <td>{item.Name}</td>
+                        <td>{item.Mobile}</td>
+                        <td>{item.Mail}</td>
+                        <td>{item.Address}</td>
+                        <td>{item.Age}</td>
+                        <td>{item.Gender}</td>
+                        <td>{item.DateRequested}</td>
+                        <td>{item.TimeRequested}</td>
+                        <td>{item.Purpose}</td>
+                        <td className="new-record-operations">
+                          {/* <Link to={`/update/${item.id}`}>
+                                        <a >
+                                            <i className="fas fa-pencil-alt fa-lg update-btn" />
+                                        </a>
+                                    </Link> */}
+                          <a>
+                            <i
+                              onClick={() => onDelete(item.id)}
+                              className="fas fa-trash-alt fa-lg delete-btn"
+                            />
+                          </a>
+
+                          <a>
+                            <i
+                              onClick={() => pushToPastAppointments(item.id)}
+                              className="fa-sharp fa-solid fa-check fa-lg tick-btn"
+                            />
+                          </a>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              )}
             </table>
+          </div>
         </div>
+      </div>
     </div>
-</div></div>
-  )
+  );
 }
--- src/Admin/PastAppointments/PastAppointments.js
+++ src/Admin/PastAppointments/PastAppointments.js
@@ -1,118 +1,195 @@
-import React,{ useState, useEffect} from 'react'
-import { db } from '../../Firebase-config/Firebase-config';
-import AppNav from '../AppointmentNav/AppointmentNav'
+import React, { useState, useEffect } from "react";
+import { db } from "../../Firebase-config/Firebase-config";
+import AppNav from "../AppointmentNav/AppointmentNav";
 
-export default function PastAppointments( props ) {
-
-
+export default function PastAppointments(props) {
   const [data, setData] = useState([]);
+  const [sortedData, setSortedData] = useState([]);
+  const [sort, setSort] = useState(false);
 
-    // const {Address,DateRequested,Age,Gender,Mail,Mobile,Name,Purpose,TimeRequested} = data;
-    
-    useEffect(()=>{
-        props.setNavShow(false)
-    },[props.setNavShow])
+  // const {Address,DateRequested,Age,Gender,Mail,Mobile,Name,Purpose,TimeRequested} = data;
 
-    useEffect(() => {
-        db.collection('PastAppointment').onSnapshot((snapshot) => {
-            const PastAppointments = [];
-            snapshot.forEach((doc) => {
-                PastAppointments.push({ ...doc.data(), id: doc.id });
-            });
+  useEffect(() => {
+    props.setNavShow(false);
+  }, [props.setNavShow]);
 
-            setData(PastAppointments);
-            // console.log(data);
-        })
-    }, []);
+  useEffect(() => {
+    db.collection("PastAppointment").onSnapshot((snapshot) => {
+      const PastAppointments = [];
+      snapshot.forEach((doc) => {
+        PastAppointments.push({ ...doc.data(), id: doc.id });
+      });
 
-    const onDelete = (id) => {
-        if (window.confirm("Are you sure you want to delete?")) {
-            db.collection('PastAppointment').doc(id).delete();
-        }
+      setData(PastAppointments);
+      // console.log(data);
+    });
+  }, []);
+
+  const onDelete = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      db.collection("PastAppointment").doc(id).delete();
     }
+  };
 
-    // const appointmnetCollectionRef = collection(db,"AccAppointment");
+  // const appointmnetCollectionRef = collection(db,"AccAppointment");
 
-    // const pushToAccAppointments = async (id) => {
-    //     await addDoc(appointmnetCollectionRef ,{id, Name, Age, Gender, Mobile, Mail, Address, Purpose, DateRequested, TimeRequested})
-    // }
-
+  // const pushToAccAppointments = async (id) => {
+  //     await addDoc(appointmnetCollectionRef ,{id, Name, Age, Gender, Mobile, Mail, Address, Purpose, DateRequested, TimeRequested})
+  // }
+  const handleChange = (e) => {
+    setSort(true);
+    db.collection("PastAppointment")
+      .orderBy(`${e.target.value}`)
+      .onSnapshot((snapshot) => {
+        let sortedData = [];
+        snapshot.forEach((snap) => {
+          sortedData.push({ ...snap.data(), id: snap.id });
+        });
+        setSortedData(sortedData);
+      });
+  };
+  const handleReset = () => {
+    setSort(false);
+  };
 
   return (
-    <div><div className="table-outer-container">
-    <div className='emp-div-nav'><h1>Appointments</h1></div>
-    <div className="table-inner-container1">
-        <div className="table-inner-container2">
+    <div>
+      <div className="table-outer-container">
+        <div className="emp-div-nav">
+          <h1>Appointments</h1>
+        </div>
+        <div className="table-inner-container1">
+          <div className="table-inner-container2">
             <div className="table-heading">
-                <h1>{<AppNav/>}</h1>
+              <h1>{<AppNav />}</h1>
             </div>
-
+            <label>Sort By:</label>
+            <select
+              className="dropdown"
+              name="colValue"
+              onChange={handleChange}
+            >
+              <option>Please Select</option>
+              <option value="Name">Name</option>
+              <option value="TimeRequested">Time Requested</option>
+              <option value="DateRequested">Date Requested</option>
+            </select>
+            <button className="btn btn-reset" onClick={handleReset}>
+              Reset
+            </button>
+            <br />
             <table className="table-content">
-
-                <thead className='thead-dark'>
-                    <tr>
-                        <th >No.</th>
-                        <th >Name</th>
-                        <th >Mobile</th>
-                        <th >Mail</th>
-                        <th >Address</th>
-                        <th >Age</th>
-                        <th >Gender</th>
-                        <th >Date Requested</th>
-                        <th >Time Requested</th>
-                        <th >Purpose</th>
-                        <th >Action</th>
-                    </tr>
-                </thead>
-
+              <thead className="thead-dark">
+                <tr>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Mobile</th>
+                  <th>Mail</th>
+                  <th>Address</th>
+                  <th>Age</th>
+                  <th>Gender</th>
+                  <th>Date Requested</th>
+                  <th>Time Requested</th>
+                  <th>Purpose</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              {!sort && (
                 <tbody>
-                    {
-                    data.map((item, index) => {
-                        let even = false
+                  {data.map((item, index) => {
+                    let even = false;
 
-                            if((index+1)%2===0){
-                                even = true
-                            }
-                            else{
-                                even = false
-                            }
+                    if ((index + 1) % 2 === 0) {
+                      even = true;
+                    } else {
+                      even = false;
+                    }
 
-                        return (
-                            <tr key={item.id} className={`${even ? 'even-row':''}`}>
-                                <th >{index + 1}</th>
-                                <td>{item.Name}</td>
-                                <td>{item.Mobile}</td>
-                                <td>{item.Mail}</td>
-                                <td>{item.Address}</td>
-                                <td>{item.Age}</td>
-                                <td>{item.Gender}</td>
-                                <td>{item.DateRequested}</td>
-                                <td>{item.TimeRequested}</td>
-                                <td>{item.Purpose}</td>
-                                <td className='new-record-operations'>
-                                    {/* <Link to={`/update/${item.id}`}>
+                    return (
+                      <tr key={item.id} className={`${even ? "even-row" : ""}`}>
+                        <th>{index + 1}</th>
+                        <td>{item.Name}</td>
+                        <td>{item.Mobile}</td>
+                        <td>{item.Mail}</td>
+                        <td>{item.Address}</td>
+                        <td>{item.Age}</td>
+                        <td>{item.Gender}</td>
+                        <td>{item.DateRequested}</td>
+                        <td>{item.TimeRequested}</td>
+                        <td>{item.Purpose}</td>
+                        <td className="new-record-operations">
+                          {/* <Link to={`/update/${item.id}`}>
                                         <a >
                                             <i className="fas fa-pencil-alt fa-lg update-btn" />
                                         </a>
                                     </Link> */}
-                                    <a >
-                                        <i onClick={() => onDelete(item.id)} className="fas fa-trash-alt fa-lg delete-btn" />
-                                    </a>
-                                    {/* <Link to='/AccAppointments'>
+                          <a>
+                            <i
+                              onClick={() => onDelete(item.id)}
+                              className="fas fa-trash-alt fa-lg delete-btn"
+                            />
+                          </a>
+                          {/* <Link to='/AccAppointments'>
                                         <a onClick={() => pushToAccAppointments(item.id)}>
                                             <i className="fa-sharp fa-solid fa-check fa-lg tick-btn" />
                                         </a>
                                     </Link> */}
-
-                                </td>
-                            </tr>
-                        );
-                    })
-                    }
+                        </td>
+                      </tr>
+                    );
+                  })}
                 </tbody>
+              )}
+              {sort && (
+                <tbody>
+                  {sortedData.map((item, index) => {
+                    let even = false;
+
+                    if ((index + 1) % 2 === 0) {
+                      even = true;
+                    } else {
+                      even = false;
+                    }
+
+                    return (
+                      <tr key={item.id} className={`${even ? "even-row" : ""}`}>
+                        <th>{index + 1}</th>
+                        <td>{item.Name}</td>
+                        <td>{item.Mobile}</td>
+                        <td>{item.Mail}</td>
+                        <td>{item.Address}</td>
+                        <td>{item.Age}</td>
+                        <td>{item.Gender}</td>
+                        <td>{item.DateRequested}</td>
+                        <td>{item.TimeRequested}</td>
+                        <td>{item.Purpose}</td>
+                        <td className="new-record-operations">
+                          {/* <Link to={`/update/${item.id}`}>
+                                        <a >
+                                            <i className="fas fa-pencil-alt fa-lg update-btn" />
+                                        </a>
+                                    </Link> */}
+                          <a>
+                            <i
+                              onClick={() => onDelete(item.id)}
+                              className="fas fa-trash-alt fa-lg delete-btn"
+                            />
+                          </a>
+                          {/* <Link to='/AccAppointments'>
+                                        <a onClick={() => pushToAccAppointments(item.id)}>
+                                            <i className="fa-sharp fa-solid fa-check fa-lg tick-btn" />
+                                        </a>
+                                    </Link> */}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              )}
             </table>
+          </div>
         </div>
+      </div>
     </div>
-</div></div>
-  )
+  );
 }
